2bcc61e089f8b48ea9456fed01ed4d1d31a57314
nalivaykom <nalim2@uw.edu>
2022-05-17 02:01:36 -0700

    added request to email message in register

--- routes/register.js
+++ routes/register.js
@@ -71,15 +71,15 @@ router.get('/verify/:userEmail', (request, response) => {
         // })
         pool.query(theQuery)
             .then(result => {
-                //result.f
+                
                 //request.memberid = result.rows[0].memberid
-                // response.status(200).send({
-                //     message: result
-                // })
+                response.status(200).send({
+                    message: result.fields
+                })
                 
                 next()
             })
-            .catch((err) => {
+            .catch((error) => {
                 response.status(400).send({
                     message: "verification failed " + error.constraint
                 })

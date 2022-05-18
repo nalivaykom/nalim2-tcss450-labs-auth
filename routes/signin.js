9a3cdafde9b87c7bd4f3cb8b20d016f65d17408b
nalivaykom <nalim2@uw.edu>
2022-05-17 21:16:18 -0700

    added request to email message in register

--- routes/signin.js
+++ routes/signin.js
@@ -140,7 +140,7 @@ router.get('/', (request, response, next) => {
                 //package and send the results
                 response.json({
                     success: true,
-                    message: 'Authentication successful! ' + theQuery2,
+                    message: 'Authentication successful! ' + email,
                     token: token
                 })
             } else {

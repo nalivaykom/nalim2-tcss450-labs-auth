1a177bf05714955dfb30389f3e85810bd68545de
nalivaykom <nalim2@uw.edu>
2022-05-17 20:34:05 -0700

    added request to email message in register

--- routes/register.js
+++ routes/register.js
@@ -88,11 +88,10 @@ router.get('/verify/:userEmail?', (request, response, next) => {
     let userEmail = request.params.userEmail
     let theQuery = "UPDATE members SET verification = '1' WHERE email = '" + userEmail + "'" 
     pool.query(theQuery)
-        .then(//result => {
-            //response.status(200).send({
-                //message:"Congratulations: your email has been verified. You may now log in!"
-            //})
-        //}
+        .then(
+            response.status(200).send({
+                message:"Congratulations: your email has been verified. You may now log in!"
+            })
         )
         .catch(error => {
             response.status(400).send({ 
